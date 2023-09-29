04be09d3aec71992c458b43717f5c296aa632a9b
Яков Клейменов <yakovkleimenov@yandex.ru>
2023-09-29 22:27:27 +0400

    Added changes

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import requests
+import time
 
 from telegram import Bot
 from dotenv import load_dotenv
@@ -21,10 +22,9 @@ def get_homework_status(tg_chat_id, tg_token, dm_token):
                                 headers=header,
                                 params=params)
         response.raise_for_status()
-        response_data = response.json()
-        print(response.text)
-        if 'new_attempts' in response_data:
-            new_attempt = response_data['new_attempts'][0]
+        info_checkout = response.json()
+        if 'new_attempts' in info_checkout:
+            new_attempt = info_checkout['new_attempts'][0]
             is_negative = new_attempt['is_negative']
             lesson_title = new_attempt['lesson_title']
             lesson_url = new_attempt['lesson_url']
@@ -34,9 +34,9 @@ def get_homework_status(tg_chat_id, tg_token, dm_token):
                 result = positive_text
             massage = f'Работа "{lesson_title}" {result}. {lesson_url}'
             bot.send_message(tg_chat_id, massage)
-            timestamp = response_data['last_attempt_timestamp']
+            timestamp = info_checkout['last_attempt_timestamp']
         else:
-            timestamp = response_data['timestamp_to_request']
+            timestamp = info_checkout['timestamp_to_request']
 
 
 def main():
@@ -45,27 +45,31 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-c', '--chat_id',
-                        type=int, help='The chat ID to use.')
+                        type=int, help='The chat ID to use.',
+                        default=os.environ.get('TG_CHAT_ID'))
 
     args = parser.parse_args()
 
-    tg_chat_id = os.environ.get('TG_CHAT_ID')
-
-    if not tg_chat_id:
-        tg_chat_id = args.chat_id
+    tg_chat_id = args.chat_id
 
     tg_token = os.environ['TELEGRAM_TOKEN']
     dm_token = os.environ['DEVMAN_TOKEN']
+    max_retries = 5
+    retry_count = 0
 
-    while True:
-        try:
-            get_homework_status(tg_chat_id, tg_token, dm_token)
-        except ConnectionError as con_error:
-            print(con_error)
-        except requests.exceptions.ReadTimeout as rt_error:
-            print(rt_error)
-        except Exception as e:
-            print(e)
+    try:
+        get_homework_status(tg_chat_id, tg_token, dm_token)
+        retry_count = 0
+    except ConnectionError as con_error:
+        print(con_error)
+        retry_count += 1
+        if retry_count >= max_retries:
+            time.sleep(30)
+            retry_count = 0
+    except requests.exceptions.ReadTimeout as rt_error:
+        print(rt_error)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
